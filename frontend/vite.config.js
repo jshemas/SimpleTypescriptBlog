b20ae18afdfc48946a05aace4b657cf0a002064a
jshemas <joshshemas@gmail.com>
2026-05-07 17:06:40 -0700

    adding steps to switch project over to react compiler

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from "vite-plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"], // if you use TypeScript
+        plugins: [
+          ["babel-plugin-react-compiler"],
+        ],
+      },
+    }),
+  ],
 })
